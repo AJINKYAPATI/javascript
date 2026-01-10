95ece34f604374f30b849efd68672e34dba35389
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-01-10 12:42:25 +0530

    Create product.js

---
+++ product.js
@@ -0,0 +1,16 @@
+export let watch={
+    pname:"titan",
+    price:5000,
+    warranty:"1 year"
+}
+
+export let watch1={
+    pname:"sonata",
+    price:4000,
+    warranty:"1 year"
+}
+
+export default function test(){
+    console.log("testing default function.....");
+    
+}
