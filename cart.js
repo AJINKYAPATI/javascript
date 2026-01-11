b7195348e2baccfe59cc93b4df65a84135475f26
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-01-11 23:45:14 +0530

    Add console logs for product details in cart.js

--- cart.js
+++ cart.js
@@ -1,8 +1,10 @@
 import {watch,watch1} from"./product.js"
 import a from"./product.js";
 
+
 console.log("Welcome to cart page");
 console.log(watch);
 console.log(watch.pname);
 console.log(watch1);
+
 a();
