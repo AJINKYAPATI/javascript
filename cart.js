a7055fd075215ee73bbaefaff35fccb30f70e8b7
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-01-13 23:27:21 +0530

    Refactor cart.js to call function 'a' once

--- cart.js
+++ cart.js
@@ -1,10 +1,7 @@
 import {watch,watch1} from"./product.js"
 import a from"./product.js";
-
-
 console.log("Welcome to cart page");
 console.log(watch);
 console.log(watch.pname);
 console.log(watch1);
-
 a();
