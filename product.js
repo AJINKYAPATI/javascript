780925cdd3940713a04c25d3251d1a4a7c8d0027
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-01-26 23:41:48 +0530

    Update product.js

--- product.js
+++ product.js
@@ -3,11 +3,13 @@ export let watch={
     price:5000,
     warranty:"1 year"
 }
+
 export let watch1={
     pname:"sonata",
     price:4000,
     warranty:"1 year"
 }
+
 export default function test(){
 console.log("testing default function.....");
     }
