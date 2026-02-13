1cf066abc1cc0a5713356cef45184427e2f975f8
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-02-13 23:54:55 +0530

    Add welcome message and log product details

--- cart.js
+++ cart.js
@@ -1,7 +1,9 @@
 import {watch,watch1} from"./product.js"
 import a from"./product.js";
+
 console.log("Welcome to cart page");
 console.log(watch);
 console.log(watch.pname);
 console.log(watch1);
+
 a();
