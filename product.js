2d50dbfbc08949d16fef229a084bd1888812297b
Ajinkya Patil <158836219+AJINKYAPATI@users.noreply.github.com>
2026-02-27 16:32:56 +0530

    Clean up formatting in product.js
    Removed extra newlines and fixed formatting in product.js.

--- product.js
+++ product.js
@@ -3,13 +3,11 @@ export let watch={
     price:5000,
     warranty:"1 year"
 }
-
 export let watch1={
     pname:"sonata",
     price:4000,
     warranty:"1 year"
 }
-
 export default function test(){
 console.log("testing default function.....");
     }
